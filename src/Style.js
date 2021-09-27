a0525135ddec4f9e81dcf8e77172c0a708d56c11
stef <stephenegbert@gmail.com>
2021-09-27 11:02:34 -0400

    setup color schemes

---
+++ src/Style.js
@@ -0,0 +1,17 @@
+const COLORS = {
+  jet: "#363636",
+  charcoal: "#33435B",
+  gold: "#CCA43B",
+  plat: #E5E5E5,
+  white: "#FFFFFF",
+  wildBlue: "#8EA4D2",
+  glauc: "#6279B8",
+  indy: "#49516F",
+  bender: "#496F5D",
+  shamrock: "#4C9F70"
+  
+}
+
+export default {
+  colors: COLORS
+}
