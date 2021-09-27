d16840ba8c2910687845b27d5cd4ffcbd3b68876
stef <stephenegbert@gmail.com>
2021-09-27 15:05:28 -0400

    pre lunch tweaking

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useTheme } from "styled-components";
 import { Wrapper } from "./styles";
-import { NavTop } from "../../components";
 
 const Home = () => {
   const theme = useTheme();
